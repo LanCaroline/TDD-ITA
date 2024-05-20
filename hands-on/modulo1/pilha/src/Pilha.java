61a0ef1de06821b3a933954ae3eb3961b2e6f0ba
LanCaroline <lana.de.sa@hotmail.com>
2024-05-20 16:14:27 -0300

    hands on parte 2

--- hands-on/modulo1/pilha/src/Pilha.java
+++ hands-on/modulo1/pilha/src/Pilha.java
@@ -1,13 +1,13 @@
 public class Pilha {
 
-    private Object elemento;
-    private int quantidade;
+    private Object[] elementos = new Object[20];
+    private int quantidade = 0;
 
     public Pilha(){}
 
 
     public boolean estaVazia() {
-        return elemento == null;
+            return quantidade == 0;
     }
 
     public int tamanhoPilha() {
@@ -15,11 +15,17 @@ public class Pilha {
     }
 
     public void empilha(Object elemento) {
-        this.elemento = elemento;
+        this.elementos[quantidade] = elemento;
         quantidade++;
     }
 
     public Object topo() {
-        return elemento;
+        return elementos[quantidade-1];
+    }
+
+    public Object desempilha() {
+        Object topo = topo();
+        quantidade--;
+        return topo;
     }
 }
--- hands-on/modulo1/pilha/src/PilhaTeste.java
+++ hands-on/modulo1/pilha/src/PilhaTeste.java
@@ -14,13 +14,27 @@ public class PilhaTeste {
     }
 
     @Test
-    public void empilhaElemento(){
+    public void empilhaUmElemento(){
         Pilha p = new Pilha();
         p.empilha("primeiro Elemento");
         assertFalse(p.estaVazia());
         assertEquals(1, p.tamanhoPilha());
         assertEquals("primeiro Elemento", p.topo());
 
+    }
+
+    @Test
+    public void empilhaDesempilha(){
+        Pilha p = new Pilha();
+        p.empilha("primeiro elemento");
+        p.empilha("segundo elemento");
+        assertFalse(p.estaVazia());
+        assertEquals(2, p.tamanhoPilha());
+        assertEquals("segundo elemento", p.topo());
+        Object desempilhado = p.desempilha();
+        assertEquals(1, p.tamanhoPilha());
+        assertEquals("primeiro elemento", p.topo());
+        assertEquals("segundo elemento", desempilhado);
     }
 
 
